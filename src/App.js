70e1e93bbcd9159da8fb3469d6ca552b9e785ad1
Stalin Maza <stalinct97@gmail.com>
2022-07-16 10:37:22 -0500

    edit app message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Basic example of React App using Docker.
+          <br/>
+          Edit file <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer>
+        <p>Made by Stalin Maza</p>
+      </footer>
     </div>
   );
 }
